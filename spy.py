2026fd19df5a99151712696a9005b2b241344a63
SAT Shahin YT <109285746+SPY1x1@users.noreply.github.com>
2022-09-16 10:01:28 +0600

    Update spy.py

--- spy.py
+++ spy.py
@@ -1,5 +1,33 @@
-import os, sys
+import os, platform
+ 
 try:
-    __import__("SpY64")._SpyX___()
-except Exception as e:
-    exit(str(e))
+ 
+        import requests
+ 
+except:
+ 
+        os.system('pip2 install requests')
+ 
+ 
+ 
+import requests
+ 
+bit = platform.architecture()[0]
+ 
+if bit == "64bit":
+ 
+        from Proxnx import _SpyX___
+ 
+        _SpyX___()
+ 
+ 
+ 
+elif bit == "32bit":
+ 
+        from SpY32 import SpY
+ 
+ 
+        SpY()
+ 
+ 
+ 
